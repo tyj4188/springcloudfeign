0d14141ec8b6a55047718dd8ec8040a16862d5e6
tyj4188 <617191529@qq.com>
2017-11-09 17:51:03 +0800

    update

--- src/main/java/com/john/Application.java
+++ src/main/java/com/john/Application.java
@@ -10,16 +10,25 @@ package com.john;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @author ihui
  * @date 2017/11/8
  */
+
+/**
+ * 使用 Hystrix 仪表盘 必须要 EnableCircuitBreaker 注解, 而且需要三个环境引入,
+ * 分别是: spring-boot-starter-actuator, spring-cloud-starter-hystrix, spring-cloud-starter-hystrix-dashboard;
+ */
 @SpringBootApplication
 @EnableEurekaClient // 服务注册
 @EnableFeignClients // 负载均衡
+@EnableCircuitBreaker // 使用 Hystrix 仪表盘 必须要加此注解
+@EnableHystrixDashboard // 启动 Hystrix 仪表盘 - 访问 /hystrix
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/john/service/ScheduleServiceGreeting.java
+++ src/main/java/com/john/service/ScheduleServiceGreeting.java
@@ -8,6 +8,7 @@
 
 package com.john.service;
 
+import com.john.hystrix.GreetingHystrix;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,13 +18,19 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author ihui
  * @date 2017/11/8
  */
-@FeignClient(value="SPRINGCLOUDAPP") // 指定服务名
+
+/**
+ *  Feign 自带 Hystrix, 需不要额外引用Jar
+ *  value - 指定服务名
+ *  fallback - 指定断路器类, 断路器类需要实现此接口
+ */
+@FeignClient(value = "SPRINGCLOUDAPP", fallback = GreetingHystrix.class)
 public interface ScheduleServiceGreeting {
     /**
      * 调用 SPRINGCLOUDAPP/greeting接口, 参数为 name
      * @param name
      * @return
      */
-    @RequestMapping(value="/greeting", method=RequestMethod.GET)
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public String greeting(@RequestParam(value = "name") String name);
 }
